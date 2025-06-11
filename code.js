6cf6c7c8177bd79554e39f90207291f677d3e24c
m-argenta <174133440+m-argenta@users.noreply.github.com>
2025-06-12 01:46:16 +0200

    Add files via upload

---
+++ code.js
@@ -0,0 +1,17 @@
+window.onload=init;
+
+function setFace(path){
+    document.getElementById('puppetFace').src=path;
+}
+
+function setWigBack(path){
+    document.getElementById('wigBack').src=path;
+}
+
+function setFlame(path){
+    document.getElementById('flame').src=path;
+}
+
+function setPin(path){
+    document.getElementById('pin').src=path;
+}
